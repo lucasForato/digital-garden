a31871131f0df2315c3a35dbd4e3746c7750e4c6
Lucas Forato <lforato@soap.health>
2024-02-02 12:52:40 -0300

    Quartz sync: Feb 2, 2024, 12:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "</> Sourcecode",
+    pageTitle: "</> SourceCode",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
